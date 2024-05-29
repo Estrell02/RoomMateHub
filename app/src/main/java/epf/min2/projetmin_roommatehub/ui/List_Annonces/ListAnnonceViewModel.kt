5b535109516ae4bbcc000a9dc76c1dbae3e7e08e
Estrella <estrella.nounamo@institutsaintjean.org>
2024-05-29 13:55:52 +0200

    images login

---
+++ app/src/main/java/epf/min2/projetmin_roommatehub/ui/List_Annonces/ListAnnonceViewModel.kt
@@ -0,0 +1,7 @@
+package epf.min2.projetmin_roommatehub.ui.List_Annonces
+
+import androidx.lifecycle.ViewModel
+
+class ListAnnonceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/epf/min2/projetmin_roommatehub/ui/List_Annonces/List_annonce_Fragment.kt
@@ -0,0 +1,32 @@
+package epf.min2.projetmin_roommatehub.ui.List_Annonces
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import epf.min2.projetmin_roommatehub.R
+
+class List_annonce_Fragment : Fragment() {
+
+    companion object {
+        fun newInstance() = List_annonce_Fragment()
+    }
+
+    private lateinit var viewModel: ListAnnonceViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list_annonce_, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListAnnonceViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
